096814ab637ba39ab4dbebc16cdbe4c0290caec9
Chaejin Kim <73324850+chaejin-jen@users.noreply.github.com>
2023-12-13 17:39:00 +0900

    Refactor: Remove unnecessary comments

--- lib/src/data/remote/api/search_api.dart
+++ lib/src/data/remote/api/search_api.dart
@@ -4,20 +4,13 @@ import 'package:sagi_jeonae_app/src/data/remote/response/search_response.dart';
 
 
 part 'search_api.g.dart';
-// 127.0.0.1
-// 192.168.2.20
-// 27.35.75.241
-// @RestApi(baseUrl: 'http://192.168.2.20:8000')
+
 @RestApi(baseUrl: 'http://192.168.0.18:8000')
-// @RestApi(baseUrl: 'http://10.0.2.2:8000')
 abstract class SearchApi {
   factory SearchApi(Dio dio, {String baseUrl}) = _SearchApi;
 
   @GET('/product_info/url')
   Future<SearchResponse?> fetchProductInfoByUrl(@Query('encoded_url') String url);
-
-  // @GET('/{category}/xxx')
-  // Future<SearchResultModel<T>> fetchProductInfoByXxx<T>(@Path('category') String category, @Query('product_name') String xxx);
 
   @GET('/medical_device/product')
   Future<SearchResponse?> fetchProductInfoByName(@Query('model_name') String productName);
